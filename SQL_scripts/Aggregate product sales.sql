fa7871843b70da0bef424c32fe31927da4c55d0f
sameer hans <48256811+sameerhans21@users.noreply.github.com>
2025-02-19 16:46:52 +0100

    Delete SQL_scripts directory

--- SQL_scripts/Aggregate product sales.sql
+++
@@ -1,7 +0,0 @@
- SELECT d.CalendarYear, d.MonthNumberOfYear, d.EnglishMonthName,
-        p.EnglishProductName AS Product, SUM(o.OrderQuantity) AS UnitsSold
- FROM dbo.FactInternetSales AS o
- JOIN dbo.DimDate AS d ON o.OrderDateKey = d.DateKey
- JOIN dbo.DimProduct AS p ON o.ProductKey = p.ProductKey
- GROUP BY d.CalendarYear, d.MonthNumberOfYear, d.EnglishMonthName, p.EnglishProductName
- ORDER BY d.MonthNumberOfYear
